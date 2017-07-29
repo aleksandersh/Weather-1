b1f585549cbebfc2dc1654af2a748b31b028405b
AleksanderSh <alexxx.inter@mail.ru>
2017-07-29 03:28:13 +0300

    Added city selection functionality in user interface

--- app/src/main/java/ru/yamblz/weather/ui/overview/OverviewPresenterImpl.java
+++ app/src/main/java/ru/yamblz/weather/ui/overview/OverviewPresenterImpl.java
@@ -2,6 +2,10 @@ package ru.yamblz.weather.ui.overview;
 
 import javax.inject.Inject;
 
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import ru.yamblz.weather.data.local.AppPreferenceManager;
 import ru.yamblz.weather.data.usecase.OverviewUseCase;
 import ru.yamblz.weather.di.scope.PerActivity;
 import ru.yamblz.weather.ui.base.BasePresenter;
@@ -10,10 +14,12 @@ import ru.yamblz.weather.ui.base.BasePresenter;
 public class OverviewPresenterImpl extends BasePresenter<OverviewContract.OverviewView> implements OverviewContract.OverviewPresenter {
 
     private OverviewUseCase useCase;
+    private AppPreferenceManager preferenceManager;
 
     @Inject
-    public OverviewPresenterImpl(OverviewUseCase useCase) {
+    public OverviewPresenterImpl(OverviewUseCase useCase, AppPreferenceManager preferenceManager) {
         this.useCase = useCase;
+        this.preferenceManager = preferenceManager;
     }
 
     @Override
@@ -21,16 +27,28 @@ public class OverviewPresenterImpl extends BasePresenter<OverviewContract.Overvi
         getView().showLoading();
         getCompositeDisposable().add(
                 useCase.loadCurrentWeather(lat, lng, force)
-                .subscribe(
-                        weatherResponse -> {
-                            getView().hideLoading();
-                            getView().displayWeatherData(weatherResponse);
-                        },
-                        err -> {
-                            getView().showError();
-                            getView().hideLoading();
-                        }
-                )
+                        .subscribe(
+                                weatherResponse -> {
+                                    getView().hideLoading();
+                                    getView().displayWeatherData(weatherResponse);
+                                },
+                                err -> {
+                                    getView().showError();
+                                    getView().hideLoading();
+                                }
+                        )
         );
+    }
+
+    @Override
+    public void requestInitialData() {
+        getView().showLoading();
+        Single.fromCallable(() -> preferenceManager.getLocation())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(location -> {
+                    getView().hideLoading();
+                    getView().setCurrentLocation(location);
+                });
     }
 }
